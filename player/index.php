68fa46db9e5fcbb043804183d8068a8bdd0b7913
Brandon Shafer <bashafer@Brandons-MacBook-Air-2.local>
2013-04-21 21:17:07 -0400

    trying

--- player/index.php
+++ player/index.php
@@ -8,7 +8,7 @@ $mpd = new mpd($mpd_host,$mpd_port);
 if($mpd->connected != 1){
 	echo "Not Connected to Server";
         echo $mpd->errStr;
-        echo $mpd->$dummy;
+        var_dump($mpd->$dummy);
 	exit(1);
 }
 
